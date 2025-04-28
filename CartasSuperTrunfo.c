22b6dc47da40fee1b491d55dc33c0d73589b0a7d
Gustavo Conti <gust.conti@gmail.com>
2025-04-28 01:45:52 -0300

    Nivel Mestre - concluído

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,9 +18,6 @@ typedef struct
 
 Cidade cadastrarCarta(Cidade cidade)
 {
-    printf("Cadastre a carta!\n");
-    printf("\n");
-
     // Entrada de dados para a cidade
     printf("Digite o código da carta (ex: A01): ");
     scanf("%3s", cidade.codigoDaCarta); // Limita a entrada a 3 caracteres
@@ -35,59 +32,84 @@ Cidade cadastrarCarta(Cidade cidade)
     while (getchar() != '\n'); // Limpa o buffer
 
     printf("Digite a população: ");
-    if (scanf("%d", &cidade.populacao) != 1) {
+    if (scanf("%d", &cidade.populacao) != 1)
+    {
         printf("Entrada inválida! Tente novamente.\n");
-        while (getchar() != '\n'); // Limpa o buffer
+        while (getchar() != '\n')
+            ; // Limpa o buffer
     }
 
     printf("Digite a área (em km²): ");
-    if (scanf("%f", &cidade.area) != 1) {
+    if (scanf("%f", &cidade.area) != 1)
+    {
         printf("Entrada inválida! Tente novamente.\n");
-        while (getchar() != '\n'); // Limpa o buffer
+        while (getchar() != '\n')
+            ; // Limpa o buffer
     }
 
     printf("Digite o PIB: ");
-    if (scanf("%f", &cidade.pib) != 1) {
+    if (scanf("%f", &cidade.pib) != 1)
+    {
         printf("Entrada inválida! Tente novamente.\n");
-        while (getchar() != '\n'); // Limpa o buffer
+        while (getchar() != '\n')
+            ; // Limpa o buffer
     }
 
     printf("Digite o número de pontos turísticos: ");
-    if (scanf("%d", &cidade.numeroDePontosTuristicos) != 1) {
+    if (scanf("%d", &cidade.numeroDePontosTuristicos) != 1)
+    {
         printf("Entrada inválida! Tente novamente.\n");
-        while (getchar() != '\n'); // Limpa o buffer
+        while (getchar() != '\n')
+            ; // Limpa o buffer
     }
 
-    printf("\n");
-    printf("Carta cadastrada com sucesso!\n");
-    printf("\n");
-
     // Cálculo dos atributos derivados
-    if (cidade.area != 0) {
+    if (cidade.area != 0)
+    {
         cidade.densidadeDemografica = cidade.populacao / cidade.area;
-    } else {
+    }
+    else
+    {
         cidade.densidadeDemografica = 0;
     }
 
-    if (cidade.populacao != 0) {
+    if (cidade.populacao != 0)
+    {
         cidade.pibPerCapita = cidade.pib / cidade.populacao;
-    } else {
+    }
+    else
+    {
         cidade.pibPerCapita = 0;
     }
 
-    if (cidade.densidadeDemografica != 0) {
+    if (cidade.densidadeDemografica != 0)
+    {
         cidade.superPoder = (float)cidade.populacao + cidade.area + cidade.pib + (float)cidade.numeroDePontosTuristicos + cidade.pibPerCapita + 1 / cidade.densidadeDemografica;
-    } else {
+    }
+    else
+    {
         cidade.superPoder = 0;
     }
+
+    // Exibe os dados cadastrados
+    printf("\nCarta cadastrada com sucesso:\n");
+    printf("Código da Carta: %s\n", cidade.codigoDaCarta);
+    printf("Nome da Cidade: %s\n", cidade.nomeDaCidade);
+    printf("Estado: %c\n", cidade.estado);
+    printf("População: %d\n", cidade.populacao);
+    printf("Área: %.2f km²\n", cidade.area);
+    printf("PIB: %.2f\n", cidade.pib);
+    printf("Número de Pontos Turísticos: %d\n", cidade.numeroDePontosTuristicos);
+    printf("Densidade Demográfica: %.2f\n", cidade.densidadeDemografica);
+    printf("PIB per Capita: %.2f\n", cidade.pibPerCapita);
+    printf("Super Poder: %.2f\n", cidade.superPoder);
 
     return cidade;
 }
 
-int menuEscolhaAtributos() {
+int menuEscolhaAtributos()
+{
     int atributo;
-
-    printf("Escolha dois atributos para comparar:\n");
     printf("1. População\n");
     printf("2. Área\n");
     printf("3. PIB\n");
@@ -95,11 +117,11 @@ int menuEscolhaAtributos() {
     printf("5. Densidade Demográfica\n");
     printf("6. PIB per Capita\n");
     printf("7. Super Poder\n");
-
-    printf("Digite o número do primeiro atributo: ");
+    printf("Digite o número do atributo: ");
     scanf("%d", &atributo);
 
-    if(atributo < 1 || atributo > 7) {
+    if (atributo < 1 || atributo > 7)
+    {
         printf("Atributo inválido! Tente novamente.\n");
         return menuEscolhaAtributos(); // Chama a função novamente se o atributo for inválido
     }
@@ -107,7 +129,8 @@ int menuEscolhaAtributos() {
     return atributo;
 }
 
-float buscarAtributo(int opcao, Cidade cidade) {
+float buscarAtributo(int opcao, Cidade cidade)
+{
     switch (opcao)
     {
     case 1:
@@ -125,7 +148,7 @@ float buscarAtributo(int opcao, Cidade cidade) {
     case 5:
         return (float)cidade.densidadeDemografica;
         break;
-    case 6: 
+    case 6:
         return (float)cidade.pibPerCapita;
         break;
     case 7:
@@ -136,7 +159,8 @@ float buscarAtributo(int opcao, Cidade cidade) {
     }
 }
 
-char* buscarNomeAtributo(int opcao) {
+char *buscarNomeAtributo(int opcao)
+{
     switch (opcao)
     {
     case 1:
@@ -154,7 +178,7 @@ char* buscarNomeAtributo(int opcao) {
     case 5:
         return "Densidade Demográfica";
         break;
-    case 6: 
+    case 6:
         return "PIB per Capita";
         break;
     case 7:
@@ -167,7 +191,8 @@ char* buscarNomeAtributo(int opcao) {
 
 int compararAtributos(float atributoCidade1, float atributoCidade2)
 {
-    if (atributoCidade1 == atributoCidade2) {
+    if (atributoCidade1 == atributoCidade2)
+    {
         return -1; // Empate
     }
     return atributoCidade1 > atributoCidade2;
@@ -177,14 +202,21 @@ void iniciarJogo()
 {
     // Declaração de variáveis
     Cidade cidade1, cidade2;
+
+    printf("\n==================== Cadastro de Cartas ==========================\n");
+
+    printf("\n> Cadastre a primeira carta:\n");
     cidade1 = cadastrarCarta(cidade1); // Chama a função para cadastrar a primeira carta
+    printf("\n> Cadastre a segunda carta:\n");
     cidade2 = cadastrarCarta(cidade2); // Chama a função para cadastrar a segunda carta
+
+    printf("\n==================== Escolha de Atributos ==========================\n");
 
     // Escolher atributos a comparar
     int opcao1, opcao2;
-    printf("Escolha o primeiro atributo para comparar:\n");
+    printf("\n> Escolha o primeiro atributo a ser compararado:\n");
     opcao1 = menuEscolhaAtributos();
-    printf("Escolha o primeiro atributo para comparar:\n");
+    printf("\n> Escolha o segundo atributo a ser compararado:\n");
     opcao2 = menuEscolhaAtributos();
 
     cidade1.atributoSelecionado1 = buscarAtributo(opcao1, cidade1);
@@ -192,58 +224,65 @@ void iniciarJogo()
     cidade2.atributoSelecionado1 = buscarAtributo(opcao1, cidade2);
     cidade2.atributoSelecionado2 = buscarAtributo(opcao2, cidade2);
     float somaAtributosCidade1 = cidade1.atributoSelecionado1 + cidade1.atributoSelecionado2;
-    float somaAtributosCidade2 = cidade2.atributoSelecionado1 + cidade2.atributoSelecionado2;    
-    char* nomeAtributo1 = buscarNomeAtributo(opcao1);
-    char* nomeAtributo2 = buscarNomeAtributo(opcao2);
+    float somaAtributosCidade2 = cidade2.atributoSelecionado1 + cidade2.atributoSelecionado2;
+    char *nomeAtributo1 = buscarNomeAtributo(opcao1);
+    char *nomeAtributo2 = buscarNomeAtributo(opcao2);
     int comparacao1 = compararAtributos(cidade1.atributoSelecionado1, cidade2.atributoSelecionado1);
     int comparacao2 = compararAtributos(cidade1.atributoSelecionado2, cidade2.atributoSelecionado2);
     int comparacaoSoma = compararAtributos(somaAtributosCidade1, somaAtributosCidade2);
 
-    printf("\n=====================================================\n");
+    printf("\n==================== Comparação de Cartas ==========================\n");
 
-    printf("Comparação de cartas:\n");
-    printf("Comparando %s\n", nomeAtributo1);
-    
+    printf("\nComparando %s: \n", nomeAtributo1);
     if (comparacao1 == -1)
     {
-        printf("%s: Empate!\n");
+        printf("%s: Empate!\n", nomeAtributo1);
     }
-    else {
+    else
+    {
         printf("%s: Carta %i venceu (%i)\n", nomeAtributo1, comparacao1 ? 1 : 2, comparacao1);
     }
-    
-    printf("Comparando %s\n", nomeAtributo2);
+
+    printf("\nComparando %s: \n", nomeAtributo2);
     if (comparacao2 == -1)
     {
-        printf("%s: Empate!\n");
+        printf("%s: Empate!\n", nomeAtributo2);
     }
-    else {
+    else
+    {
         printf("%s: Carta %i venceu (%i)\n", nomeAtributo2, comparacao2 ? 1 : 2, comparacao2);
     }
+
+    printf("\n=================== Comparando Soma dos Atributos ===================\n");
+
     if (comparacaoSoma == -1)
     {
-        printf("Soma dos Atributos: Empate!\n");
+        printf("\nSoma dos Atributos: Empate!\n");
     }
-    else {
-        printf("Soma dos Atributos: Carta %i venceu (%i)\n", comparacaoSoma ? 1 : 2, comparacaoSoma);
+    else
+    {
+        printf("\nSoma dos Atributos: Carta %i venceu (%i)\n", comparacaoSoma ? 1 : 2, comparacaoSoma);
     }
 
-    printf("=====================================================\n");
+    printf("\n============================= Resultado =============================\n");
 
-    if (comparacaoSoma == 1) {
-        printf("RESULTADO FINAL: A carta 1 venceu!\n", cidade1.codigoDaCarta);
+    if (comparacaoSoma == 1)
+    {
+        printf("\nRESULTADO FINAL: A carta 1 venceu!\n", cidade1.codigoDaCarta);
     }
-    else if (comparacaoSoma == 0) {
-        printf("RESULTADO FINAL: A carta 2 venceu!\n", cidade2.codigoDaCarta);
+    else if (comparacaoSoma == 0)
+    {
+        printf("\nRESULTADO FINAL: A carta 2 venceu!\n", cidade2.codigoDaCarta);
     }
-    else {
-        printf("RESULTADO FINAL: Empate!\n");
+    else
+    {
+        printf("\nRESULTADO FINAL: Empate!\n");
     }
 }
 
 void exibirRegras()
 {
-    printf("Regras do Jogo:\n");
+    printf("> Regras do Jogo:\n");
     printf("1. O jogador cadastra duas cartas.\n");
     printf("2. O jogador escolhe dois atributos a serem comparados.\n");
     printf("3. O jogador com a maior soma dos atributos vence a rodada.\n");
@@ -259,11 +298,12 @@ void sair()
 void menu()
 {
     int opcao;
-    printf("Escolha uma opção:\n");
+    printf("> Escolha uma opção:\n");
     printf("1. Iniciar Jogo\n");
     printf("2. Exibir Regras\n");
     printf("3. Sair\n");
     scanf("%d", &opcao);
+
     switch (opcao)
     {
     case 1:
@@ -271,7 +311,7 @@ void menu()
         break;
     case 2:
         exibirRegras(); // Chama a função para exibir as regras
-        return menu(); // Chama a função menu novamente
+        return menu();  // Chama a função menu novamente
         break;
     case 3:
         sair(); // Chama a função para sair do jogo
